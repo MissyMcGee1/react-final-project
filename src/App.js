019cb36852b0c3d7f658fca4030efa0905bd9a53
MissyMcGee1 <130253588+MissyMcGee1@users.noreply.github.com>
2023-07-07 10:27:47 +1200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/MissyMcGee1/react-final-project.git"
+          target="_blank"
+        >
+          Open source code
+        </a>{" "}
+        by Missy McGee
+      </footer>
+    </div>
+  );
 }
